06602ed82e54f345360adc2fcb3d7747e312ea90
unknown <lilyalwe123@gmail.com>
2018-11-13 21:15:18 +0200

    python

--- 00_while_loope.py
+++ 00_while_loope.py
@@ -5,10 +5,18 @@ while counter<10:
     counter+= 1
 else:
     print("after loop",counter)
+print("end of program")
 
+print("-------------------------------------------------------")
 
-print("oof program")
-## yalwe
+count=5
+while count<10:
+    print("place in loop",count)
+    count+= 1
+else:
+    print("place after a loop",count)
+print("end of program")
+
 
 
     
