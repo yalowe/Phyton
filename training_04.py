0f58903316e6cdf6854f460ae663672870f81d17
Yalowe <lilyalwe123@gmail.com>
2019-01-02 14:07:27 +0200

    python

--- training_04.py
+++ training_04.py
@@ -3,7 +3,7 @@ def averag(mis1):
     multi=mis1*4
     smaller=mis1-2
     inte=int(mis1/3)
-    inte1=mis1%3
+    inte1=mis1%3 # mis1 = 5 ---> 5 / 3 = 1.66666667 --> 5 % 3 = 0.66666667 * 3 = 2 
     print(multi)
     print(smaller)
     print(inte)
